f8d2ec94e148b15948511ad33f7c4daac0f3b3dc
Arkainoh <arkainoh@gmail.com>
2016-09-19 12:32:03 +0900

    Basic structure is constructed.

--- src/BT.java
+++ src/BT.java
@@ -1,12 +1,21 @@
 
 public class BT {
 	private String str; //( parent , left child , right child )
+	private Node root;
+	
 	private class Node {
-		
+		int data; // should contain any kinds of data type later
+		Node left;
+		Node right;
 	}
 	public BT(String str) {
 		this.str = str;
+		
 	}
 	
+	private static BT parseBT(String str) {
+		BT resultBT = new BT(str);
+		return resultBT;
+	}
 	
 }
